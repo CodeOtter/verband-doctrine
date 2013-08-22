213af65d3c99753baad6764aa930fa312a6f57f0
Patrick Ryan <contact@ghetmedia.com>
2013-08-22 11:41:20 -0400

    Adding Query mock object because final is ugh

---
+++ Mock/Query.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Verband\Doctrine\Mock;
+
+/**
+ * Prepares Doctrine to integrate with the Framework
+ */
+class Query {
+
+    public function getResult() {
+        return true;
+    }
+}
